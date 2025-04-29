52c0630abc503c04a48dafa930e0fbd3f9533c3d
jammmin02 <jmkm1201@naver.com>
2025-04-29 12:42:22 +0900

    tesk

---
+++ class_study/0422/php_test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<?php 
+  echo "안녕하세요 " . $_POST['username'] . "<br>";
+  echo "테스트중입니다 <br>";
+  echo "<div id='bar'>안녕하세요</div>";
+?>
+
+</body>
+</html>
